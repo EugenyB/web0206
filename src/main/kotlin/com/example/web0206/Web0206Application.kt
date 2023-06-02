8adca0ec3dd914d88d893f6659691adb626471dd
eugenyb <eberkunsky@gmail.com>
2023-06-02 18:37:06 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/web0206/Web0206Application.kt
@@ -0,0 +1,11 @@
+package com.example.web0206
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Web0206Application
+
+fun main(args: Array<String>) {
+    runApplication<Web0206Application>(*args)
+}
---
+++ src/test/kotlin/com/example/web0206/Web0206ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.web0206
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Web0206ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
